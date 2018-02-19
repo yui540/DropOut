d4c7acdda6817f90b43a6f6656b17f20affaa865
yuki540 <tabletennis540@gmail.com>
2018-02-20 00:09:08 +0900

    初期環境を構築

---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+console.log('##############################################')
+console.log(' どーもーココロヤミです')
+console.log(' そんなにこのサイトの仕組みが知りたいの？')
+console.log(' ↓ ここにコードがあるから、Starつけてよね❤️')
+console.log(' https://github.com/yuki540net/DropOut')
+console.log('##############################################')
