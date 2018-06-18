3ebc80d9e043a656c3522551eed770238bf5453b
yuki540 <tabletennis540@gmail.com>
2018-06-18 10:55:16 +0900

    fix: v1~v4を非公開に

--- src/scripts/config/history.js
+++ src/scripts/config/history.js
@@ -3,25 +3,25 @@ export default [
     "version": "1.0.0",
     "title": "幻想",
     "thumb": "./images/history/v1.png",
-    "link": "http://v1.yuki540.com"
+    "link": "#"
   },
   {
     "version": "2.0.0",
     "title": "サテラ",
     "thumb": "./images/history/v2.png",
-    "link": "http://v2.yuki540.com"
+    "link": "#"
   },
   {
     "version": "3.0.0",
     "title": "結衣",
     "thumb": "./images/history/v3.png",
-    "link": "http://v3.yuki540.com"
+    "link": "#"
   },
   {
     "version": "4.0.0",
     "title": "共依存",
     "thumb": "./images/history/v4.png",
-    "link": "http://v4.yuki540.com"
+    "link": "#"
   },
   {
     "version": "5.0.0",
